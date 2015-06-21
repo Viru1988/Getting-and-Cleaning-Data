ad7ae264a72fc32e85823d865f8574bff046bfb5
viru1988 <vijaycse@live.in>
2015-06-21 22:47:30 +0530

    Course Project files modified

--- CodeBook.md
+++ CodeBook.md
@@ -1,1 +1,37 @@
 
+###CodeBook
+
+###DataSource
+
+The experiments have been carried out with a group of 30 volunteers within an age bracket of 19-48 years. Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone (Samsung Galaxy S II) on the waist. Using its embedded accelerometer and gyroscope, we captured 3-axial linear acceleration and 3-axial angular velocity at a constant rate of 50Hz. The experiments have been video-recorded to label the data manually. The obtained dataset has been randomly partitioned into two sets, where 70% of the volunteers was selected for generating the training data and 30% the test data. 
+This dataset is derived from Human Activity Recognition Using Smartphones Dataset.
+
+##Source Files:
+
+* 'README.txt'
+* 'features_info.txt': Shows information about the variables used on the feature vector.
+* 'features.txt': List of all features like mean ,std,coeff of accelerometers.
+* 'activity_labels.txt': Links the class labels with their activity name,contains the list of activities.
+* 'train/X_train.txt': Training set,Records for all the trained  test variables in Features.
+* 'train/y_train.txt': Training labels.
+* 'test/X_test.txt': Test set., Records for all the variables in Features.
+* 'test/y_test.txt': Test labels.
+*subject_train.txt: will give you the list with subjectID from  1to 30 , these ID belong to the group of people to whom the survey conducted with respect to training.
+*subject_test.txt: will give you the list with subjectID from  1to 30 , these ID belong to the group of people to whom the survey conducted with respect to test.
+
+###Transformations
+*Training and test data set rows were merged and then a unified data set created from the source files.
+*Measurements were extracted for mean, standard deviation for each measurement.
+*variable/column names were labeled with descriptive cleaner names.
+*Results were output as an indepenent tidy data set at TidyData.txt
+
+###run_analysis.R implements the above steps:
+*Require reshape2  librareis.
+*Load both test and train data which contains 2947 and 7352 respectively.
+*Load the features and activity labels.
+*Extract the mean and standard deviation column names and data.
+*Merge data set.
+*Melt the dataset and cast to get the  average of each variable for each activity and each subject.
+
+
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,59 @@
 
+library(reshape2)
+library(plyr)
+#get all the activity from activity label file.
+activity_labels<-read.table("activity_labels.txt",col.names=c("activity_id","activity_name"))
+#getting all the features obtained from the device
+features<-read.table("features.txt")
+#fetching only the fetures name
+feature_names<-features[,2]
+#reading records from test & Training files
+test_data<-read.table("X_test.txt")
+train_data<-read.table("X_train.txt")
+
+#renaming the column names with feature details in test and training data.
+colnames(test_data)<-feature_names
+colnames(train_data)<-feature_names
+
+#gettign the list of subjectids from Training and testing data.
+train_subject<-read.table("subject_train.txt")
+test_subject<-read.table("subject_test.txt")
+
+#renaming the column name as SubjectId for both training and testing results.
+colnames(train_subject)<-"subject_id"
+colnames(test_subject)<-"subject_id"
+
+#getting the data from test and training detail with respect to their Activities.
+test_activity<-read.table("y_test.txt")
+train_activity<-read.table("y_train.txt")
+
+#replacing all the column name with activity ID eg.walking,running,etc
+colnames(test_activity)<-"activity_id"
+colnames(train_activity)<-"activity_id"
+
+#binding the activityid and subjectid column to DataFrame.
+test_data<-cbind(test_subject,test_activity,test_data)
+train_data<-cbind(train_subject,train_activity,train_data)
+
+#bring both Taining and Test dataset as one
+ttData<-rbind(test_data,train_data)
+
+#grouping all column based on Mean and std
+allmean<-grep("mean",names(ttData),ignore.case=TRUE)
+mean_data<-names(ttData)[allmean]
+allstd<-grep("std",names(ttData),ignore.case=TRUE)
+std_data<-names(ttData)[allstd]
+
+#fetching only mean and std values and store it in different data frame
+msdata<-ttData[,c("subject_id","activity_id",mean_data,std_data)]
+
+#merge data between activity types and mean std data with respect to activity id
+mergeddata<-merge(activity_labels,msdata,by.x="activity_id",by.y="activity_id",all=TRUE)
+
+#all the mean and std data with respect to all the below conditions were melted to avoid unwanted data
+meltdata<-melt(mergeddata,id=c("activity_id","activity_name","subject_id"))
+castdata<-dcast(meltdata,activity_id+activity_name+subject_id~variable,mean)
+
+#writing  independent tidy data set with the average of each variable for each activity and each subject into a text file
+write.table(castdata,file="finaldata.txt",row.names=FALSE)
+
